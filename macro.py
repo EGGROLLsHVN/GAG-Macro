31268f03ee00091db3b00fcf37f2b3e2b718d40d
EGGROLLS <victor.chenghs@gmail.com>
2025-05-20 22:09:03 -0400

    Normal Commit

--- macro.py
+++ macro.py
@@ -1,67 +1,30 @@
-seeds = {
-    # Seed Shop:
-    "Carrot": False,
-    "Strawberry": False,
-    "Blueberry": False,
-    "OrangeTulip": False,
-    "TomatoSeed": False,
-    "CornSeed": False,
-    "WatermelonSeed": False,
-    "PumpkinSeed": False,
-    "AppleSeed": False,
-    "BambooSeed": False,
-    "CoconutSeed": False,
-    "CactusSeed": False,
-    "DragonFruitSeed": False,
-    "MangoSeed": False,
-    "GrapeSeed": False,
-    "MushroomSeed": False,
-    "PepperSeedCacaoSeed": False,
-    "BeanstalkSeed": False,
-
-    # Bloodmoon Shop:
-    "MysteriousCrate": False,
-    "NightEgg": False,
-    "NightSeedPack": False,
-    "BloodBananaSeed": False,
-    "MoonMelonSeed": False,
-    "StarCaller": False,
-    "BloodKiwi": False,
-    "BloodHedgehog": False,
-    "BloodOwl": False,
-
-    # Gear:
-    "WateringCan": False,
-    "Trowel": False,
-    "RecallWrench": False,
-    "BasicSprinkler": False,
-    "AdvancedSprinkler": False,
-    "GodlySprinkler": False,
-    "LightningRod": False,
-    "MasterSprinkler": False,
-    "FavoriteTool": False,
-
-    # Eggshop:
-    "All": False
-}
+import pyautogui
+import time
 
 class Macro():
-    def __init__(self, seed_data):
+    def __init__(self, seed_data, runCount, bool = False):
         self.seed_data = seed_data  # Store the seeds dictionary
+        self.macroCounter = runCount
+        self.counter = bool
 
     def run(self):
-        if self.seed_data.get("Carrot", False):
-            self.carrotCollect()
+        self.start()
 
-        if self.seed_data.get("Strawberry", False):
-            self.strawberryCollect()
+        # if self.seed_data.get("Carrot", False):
+        #     self.carrotCollect()
 
+        # if self.seed_data.get("Strawberry", False):
+        #     self.strawberryCollect()
 
-    def carrotCollect():
-        print()
+    def start(self):
+        time.sleep(3)
+        print(pyautogui.position())
+
+    # def carrotCollect():
+    #     print()
     
-    def strawberryCollect():
-        print()
+    # def strawberryCollect():
+    #     print()
 
 
 
